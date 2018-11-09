297e32bfd300bc8cc88216104aacc7dc27a1dbea
zubair <zubair.haider31@gmail.com>
2018-11-09 19:29:13 +0500

    adding changes

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -22,40 +22,40 @@ class App extends React.Component {
     this.state = { loading: true };
   }
 
-  async componentWillMount() {
-    await Font.loadAsync({
-      'Segoeui-Regular': require('./Segoeui-Regular.ttf'),
-    });
-    this.setState({ loading: false });
-  }
+  // async componentWillMount() {
+  //   // await Font.loadAsync({
+  //   //   'Segoeui-Regular': require('./Segoeui-Regular.ttf'),
+  //   // });
+  //   this.setState({ loading: false });
+  // }
 
   render() {
-    const theme = {
-      fontSize: 12,
-      fontFamily: 'Segoeui-Regular',
-      colors: {
-        text: '123',
-        gray: '#DDDDDD',
-        danger: '#DDDDDD',
-      },
-    };
-    if (this.state.loading) return null;
-
-    // return (
-    //   <ThemeProvider theme={theme}>
-    //     <AppComponent />
-    //   </ThemeProvider>
-    // );
+    // const theme = {
+    //   fontSize: 12,
+    //   fontFamily: 'Segoeui-Regular',
+    //   colors: {
+    //     text: '123',
+    //     gray: '#DDDDDD',
+    //     danger: '#DDDDDD',
+    //   },
+    // };
+    // if (this.state.loading) return null;
 
     return (
-      <View>
-        <Text>Sample Text</Text>
-        <Text textTransform="uppercase">Sample Text</Text>
-        <Text fontWeight="bold">Sample Text</Text>
-        <Icon name="user" color="red" fontSize={24} />
-        <TextInput iconLeft="user" iconRight="eye" name="username" placeholder="Name" />
-      </View>
+      <ThemeProvider theme={theme}>
+        <AppComponent />
+      </ThemeProvider>
     );
+
+    // return (
+    //   <View>
+    //     <Text>Sample Text</Text>
+    //     <Text textTransform="uppercase">Sample Text</Text>
+    //     <Text fontWeight="bold">Sample Text</Text>
+    //     <Icon name="user" color="red" fontSize={24} />
+    //     <TextInput iconLeft="user" iconRight="eye" name="username" placeholder="Name" />
+    //   </View>
+    // );
   }
 }
 
