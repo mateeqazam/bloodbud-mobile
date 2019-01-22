916d5de016850bc867da11d933ef006f7a3ec673
Zubair <zubair.haider31@gmail.com>
2019-01-22 16:40:40 +0500

    worked on user location detail on map

--- app/components/views/Map/index.js
+++ app/components/views/Map/index.js
@@ -6,30 +6,47 @@
 
 import React from 'react'
 import { StyleSheet } from 'react-native'
-import MapView, { PROVIDER_GOOGLE,Marker,Callout } from 'react-native-maps'
-import Geocoder from 'react-native-geocoder'
+import MapView, { PROVIDER_GOOGLE,Marker } from 'react-native-maps'
 
 import {
   MainView
 } from './styles'
+const googleApiKey = 'AIzaSyB94Glgain12Qqgn9Vzj4nwkQiiFKWIqx8'
 
 class Map extends React.PureComponent {
 
   constructor(props) {
     super(props)
     this.state = {
-      map: '',
-      region:{
-        latitude: 31.467589,
-        longitude: 74.306995,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,},
-      markerPoint:{
-
-        latitude: 31.467589,
-        longitude: 74.306995,
-      }
     }
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const region = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: 0.0122,
+          longitudeDelta: 0.0121
+        }
+
+        this.setState({region, markerPoint: region})
+        this.getLocationDetails(position.coords.latitude,position.coords.longitude)
+      },
+      (error) => { console.log(error); },
+      {  enableHighAccuracy: true,timeout: 30000 }
+    )
+  }
+
+  getLocationDetails(latitude, longitude) {
+    let location = [];
+    url='https://maps.googleapis.com/maps/api/geocode/json?address='+ latitude + ',' +longitude + '&key=' +  googleApiKey
+    fetch(url)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      console.log('responseJson: ', responseJson);
+    });
   }
 
   render() {
@@ -37,17 +54,6 @@ class Map extends React.PureComponent {
     const styles = StyleSheet.create({
       map: mapStyle
     })
-
-    let obj = {
-      latitude: 31.467589,
-      longitude: 74.306995,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    }
-    let obj2 = {
-      latitude: 31.467589,
-      longitude: 74.306995,
-    }
 
     var mapStyle1 = [
       {
@@ -130,22 +136,11 @@ class Map extends React.PureComponent {
       }
     ]
 
-      var region1 = {
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }
-let MY_KEY='AIzaSyB94Glgain12Qqgn9Vzj4nwkQiiFKWIqx8'
-
-// Geocoder.fallbackToGoogle(MY_KEY);
-
-// use the lib as usual
-
+    const {region} = this.state
     return (
       <MainView>
-      <MapView
-        apikey={MY_KEY}
+      { region && <MapView
+        apikey={googleApiKey}
         provider={PROVIDER_GOOGLE}
         style={ styles.map }
         initialRegion={this.state.region}
@@ -153,6 +148,7 @@ let MY_KEY='AIzaSyB94Glgain12Qqgn9Vzj4nwkQiiFKWIqx8'
         showsMyLocationButton={true}
         showsUserLocation={true}
         zoomControlEnabled={true}
+        zoomEnabled = {true}
         customMapStyle={mapStyle1}
         ref={map => { this.setState({map}) }}
         onPoiClick={e=>{
@@ -165,20 +161,17 @@ let MY_KEY='AIzaSyB94Glgain12Qqgn9Vzj4nwkQiiFKWIqx8'
           }
           this.setState({markerPoint})
         }}
+        onRegionChangeComplete={e=>this.getLocationDetails(e.latitude,e.longitude)}
       >
       <Marker coordinate={this.state.markerPoint}
       title={`${this.state.markerPoint.latitude}`}
       onDragEnd={e=>console.log('chnage',e)}
       >
 </Marker>
-      </MapView>
+      </MapView>}
     </MainView>
     );
   }
 }
-
-Map.propTypes = {
-
-};
 
 export default Map;
