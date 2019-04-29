4059137035fb984b948ee787321b951afba91a5e
Muhammad Ateeq Azam <m.ateeqazam@gmail.com>
2019-04-29 15:33:25 +0500

    fix android issues

--- android/app/src/main/java/com/bloodbud/MainApplication.java
+++ android/app/src/main/java/com/bloodbud/MainApplication.java
@@ -27,7 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNGestureHandlerPackage(),
+          new RNGestureHandlerPackage(),
             new MapsPackage(),
             // new VectorIconsPackage(),
             new RNAWSCognitoPackage()
