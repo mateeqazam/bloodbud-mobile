559dc7ccdeca40dc2ac686fe0fcf431f18e70b51
Muhammad Ateeq Azam <m.ateeqazam@gmail.com>
2019-04-29 13:08:47 +0500

    upgrade packages

--- android/app/src/main/java/com/bloodbud/MainActivity.java
+++ android/app/src/main/java/com/bloodbud/MainActivity.java
@@ -1,6 +1,9 @@
 package com.bloodbud;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +14,15 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "bloodbud";
+    }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+      return new ReactActivityDelegate(this, getMainComponentName()) {
+        @Override
+        protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+        }
+      };
     }
 }
--- android/app/src/main/java/com/bloodbud/MainApplication.java
+++ android/app/src/main/java/com/bloodbud/MainApplication.java
@@ -3,6 +3,7 @@ package com.bloodbud;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 // import com.oblador.vectoricons.VectorIconsPackage;
 import com.amazonaws.RNAWSCognitoPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGestureHandlerPackage(),
             new MapsPackage(),
             // new VectorIconsPackage(),
             new RNAWSCognitoPackage()
