6109d5df8c3457cfab627249d5ca05d82e1fecc9
zubair <zubair@zubair.localdomain>
2019-04-29 16:14:23 +0500

    fixing react navigation 3 issue

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -9,39 +9,40 @@ import { ThemeProvider } from 'styled-components';
 import {
   createBottomTabNavigator,
   createStackNavigator,
+  createAppContainer,
 } from 'react-navigation';
-import Ionicons from 'react-native-vector-icons/Ionicons'
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import Settings from '../../components/views/Settings';
 import BloodRequest from '../../components/views/BloodRequest';
-import RequestForm from '../../components/views/RequestForm'
-import FormSuccess from '../../components/views/RequestForm/success'
-import Profile from '../../components/views/Profile'
-import UpdateSettings from '../../components/views/Profile/updateSettings'
+import RequestForm from '../../components/views/RequestForm';
+import FormSuccess from '../../components/views/RequestForm/success';
+import Profile from '../../components/views/Profile';
+import UpdateSettings from '../../components/views/Profile/updateSettings';
 import Notification from '../../components/views/Notification';
-import NotificationDetail from '../../components/views/Notification/notification'
+import NotificationDetail from '../../components/views/Notification/notification';
 import SearchLocation from '../../components/views/SearchLocation';
 
 const NotificationNavigator = createStackNavigator({
   Notification,
   NotificationDetail,
-})
+});
 
 const ProfileNavigator = createStackNavigator({
   Profile,
   UpdateSettings,
-})
+});
 
 const FormNavigator = createStackNavigator({
   RequestForm,
   SearchLocation,
   BloodRequest,
   FormSuccess,
-})
+});
 
 const TabNavigator = createBottomTabNavigator(
   {
     Notification: NotificationNavigator,
-    'My Requests' :Settings,
+    'My Requests': Settings,
     Profile: ProfileNavigator,
     RequestForm: FormNavigator,
   },
@@ -50,15 +51,15 @@ const TabNavigator = createBottomTabNavigator(
       tabBarIcon: ({ focused, horizontal, tintColor }) => {
         const { routeName } = navigation.state;
         let IconComponent = Ionicons;
-        let iconName
+        let iconName;
         if (routeName === 'My Requests') {
-          iconName = 'ios-options'
+          iconName = 'ios-options';
         } else if (routeName === 'RequestForm') {
-          iconName = 'ios-create'
+          iconName = 'ios-create';
         } else if (routeName === 'Notification') {
-          iconName =  `ios-notifications${focused ? '' : '-outline'}`
+          iconName = `ios-notifications${focused ? '' : '-outline'}`;
         } else if (routeName === 'Profile') {
-          iconName = 'ios-contact'
+          iconName = 'ios-contact';
         }
 
         return <IconComponent name={iconName} size={25} color={tintColor} />;
@@ -69,6 +70,6 @@ const TabNavigator = createBottomTabNavigator(
       inactiveTintColor: 'gray',
     },
   }
-)
+);
 
-export default TabNavigator
+export default createAppContainer(TabNavigator);
